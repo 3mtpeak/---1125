ac2b213cc7b45ea5d0beaa8ac51cde4d3df38695
3mtpeak <33971118+3mtpeak@users.noreply.github.com>
2017-11-25 16:54:27 +0900

    網頁檔

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>安心食譜與食材 新竹市合格食品</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<style>
+a{cursor: pointer;
+color: #FF0000;
+	
+}
+</style>
+</head>
+
+<body  >
+<table align="center" cellpadding="0" cellspacing="0" width="800px">
+<tr><th><p>安心食譜與食材 新竹市合格食品</p></th></tr>
+<tr><th>
+	<p>
+<select onChange="content.style.display='block' ">
+ <option value="請選擇食材">請選擇食譜</option>
+ <option value="紅豆花生湯圓">紅豆花生湯圓</option>
+  
+</select>
+	</p>
+</th>	
+</tr>
+<tr><th  id="content" style="display:  none;">	
+天氣涼颼颼，來一碗暖呼呼的紅豆花生湯圓，暖胃又暖心
+<br>
+<ui  >預備食材
+
+    <div style="border: thin solid ">
+    <ul ><a onClick="aa1.style.display='block'">蜜紅豆</a> 適量</ul>
+	<table id="aa1" style="display: none;" border="1">
+	<tr>	
+	<th>類別</th><th>檢體名稱</th><th>製造廠商名稱</th><th>製造廠商地址</th><th>製造廠商電話</th><th>抽驗日期</th><th>抽驗地點</th><th>抽驗地點地址</th><th>抽驗地點電話</th><th>檢驗結果</th>								</tr>		
+	<th>冰品飲料</th><th>紅豆</th><th>光華冰店</th><th>新竹市光華東街35號</th><th>03-5322318</th><th>104.6.1</th><th>光華冰店</th><th>新竹市光華二街17號</th><th>03-5356799</th><th>符合規定</th>								</tr>
+	</table>
+	
+
+    <ul><a onClick="aa2.style.display='block'">花生</a> 適量</ul>
+	<table id="aa2" style="display: none;" border="1">
+	<tr>	
+	<th>類別</th><th>檢體名稱</th><th>製造廠商名稱</th><th>製造廠商地址</th><th>製造廠商電話</th><th>抽驗日期</th><th>抽驗地點</th><th>抽驗地點地址</th><th>抽驗地點電話</th><th>檢驗結果</th>								</tr>		
+	<th>年節食品</th><th>花生</th><th>惠康百貨股份有限公司新竹明湖分公司</th><th>新竹市香山區明湖路566號</th><th>-</th><th>105.1.12</th><th>惠康百貨股份有限公司新竹明湖分公司</th><th>新竹市香山區明湖路566號</th><th>--</th><th>符合規定</th>								</tr>
+	</table>
+	
+
+    <ul><a onClick="aa3.style.display='block'">小湯圓</a> 適量</ul>
+	<table id="aa3" style="display: none;" border="1">
+	<tr>	
+	<th>類別</th><th>檢體名稱</th><th>製造廠商名稱</th><th>製造廠商地址</th><th>製造廠商電話</th><th>抽驗日期</th><th>抽驗地點</th><th>抽驗地點地址</th><th>抽驗地點電話</th><th>檢驗結果</th>								</tr>		
+	<th>湯圓</th><th>小湯圓</th><th>榮濱商號</th><th>新竹市北新街19號</th><th>03-5278618</th><th>104.12.3</th><th>阿惠冰店</th><th>新竹市光華二街17號</th><th>03-5356799</th><th>符合規定</th>								</tr>
+	
+<!--
+湯圓	小湯圓	光華冰店	新竹市光華東街35號	03-5322318	104.12.3	光華冰店	新竹市光華東街35號	03-5322318	符合規定
+湯圓	小湯圓	進財行	新竹市城北街137巷1弄53號	03-5331481	104.12.3	阿忠冰店	新竹市光華東街21號	03-5331120	符合規定
+湯圓	湯圓	榮記	新竹市武昌街64號	03-5238238	104.12.4	榮記	新竹市武昌街64號	03-5238238	符合規定
+湯圓	桂冠芝麻湯圓	桂冠實業股份有限公司	新北市中和區橋和路60號	02-23650335	104.12.8	豆戀迷豆湯品專賣	新竹市建功路37號	02-5716658	符合規定
+湯圓	小湯圓	立晉豆花	新竹市建功一路6號		104.12.8	立晉豆花	新竹市建功一路6號		符合規定
+湯圓	小湯圓	建功路豆花店	新竹市東區建功路7號		104.12.8	建功路豆花店	新竹市東區建功路7號		符合規定
+湯圓	小湯圓	友淳豆花舖	新竹市東區建中一路19號1樓		104.12.8	友淳豆花舖	新竹市東區建中一路19號1樓		符合規定
+湯圓	小湯圓	麥香苑包子饅頭店	新竹市北門街101號	03-5233009	104.12.10	麥香苑包子饅頭店	新竹市北門街101號	03-5233009	符合規定
+湯圓	小湯圓	冰店	新竹市北門街98號		104.12.10	冰店	新竹市北門街98號		符合規定-->
+	</table>
+    <ul>水 適量</ul>
+    <ul>冰糖 適量</ul>
+	</div>
+</ui>	
+<br>
+步驟
+30 分鐘<br>
+
+    小湯圓放進滾水裡以中大火煮滾脹大浮起後關火瀝出，<br>以冷開水稍微沖涼備用
+
+    取一小鍋，將水煮滾後放入花生，<br>以中火煮至沸騰後關小火將花生煮軟<br>
+
+    加入冰糖、蜜紅豆，步驟1小湯圓攪拌均勻，煮至小滾即完成<br>
+</th>	
+</tr>	
+</table>	
+</body>
+</html>
